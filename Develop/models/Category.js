9509380e22e216dcb4ebd85ca114186697727834
Milante <milante.adams@yahoo.com>
2022-10-17 20:14:11 -0400

    product model filled

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -1,19 +1,25 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
